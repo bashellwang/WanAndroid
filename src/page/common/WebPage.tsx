7ed802240ab3b108394e66b0de87208182a059c6
bashellwang <875799016@qq.com>
2022-12-15 20:43:11 +0800

    [Update] fix warning:cannot update a component while rendering a different component

--- src/page/common/WebPage.tsx
+++ src/page/common/WebPage.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import WebView from 'react-native-webview';
+import {useEffect} from 'react';
 
 export default function WebPage({route, navigation}) {
   /**
@@ -8,8 +9,11 @@ export default function WebPage({route, navigation}) {
    */
   const {title, url} = route.params;
 
-  // 设置标题
-  navigation.setOptions({title: title});
+  useEffect(() => {
+    // 设置标题
+    navigation.setOptions({title: title});
+  }, [navigation, title]);
+
   return (
     <WebView
       source={{
